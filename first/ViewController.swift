9c5d96e1f246900a6c76568baac74839b59b7bb2
Ahmad Harki Ramadhan <harkiramadhan@icloud.com>
2021-10-09 20:37:56 +0700

    Initial Commit

---
+++ first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by Ahmad Harki Ramadhan on 09/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
